e740b436278ac5e2a66ac8cb6ca6da1006492bf1
williamjohngardner <bill@williamjohngardner.com>
2017-07-31 15:57:12 -0400

    Hooking in ACF Fields in Success section

--- front-page.php
+++ front-page.php
@@ -68,7 +68,7 @@
         <div class="team-desc">
           <h2 id="team-title" class="section-title"><?php the_field( 'team_title_white_1' ) ?> <span class="orange"><?php the_field( 'team_title_orange' ) ?></span> <?php the_field( 'team_title_white_2' ) ?></h2>
           <p id="team-text" class="section-text"><?php the_field( 'team_description' ) ?> </p>
-          <h3 id="team-button" class="calltoaction-button"><?php the_field( 'team_cta_button' ) ?></h3>
+          <a href="<?php the_field( 'team_cta_button_url' ) ?>"><h3 id="team-button" class="calltoaction-button"><?php the_field( 'team_cta_button' ) ?></h3></a>
         </div>
         <div class="team-pic">
           <img src="<?php the_field('team_pic'); ?>" alt="<?php the_field( 'attorney_title' ) ?>">
@@ -86,14 +86,16 @@
     <section class="calltoaction">
       <div class="container">
         <div id="calltoaction2">
-            <h3>READY TO SCHEDULE YOUR <span class="orange">FREE</span> CONSULTATION? CALL <span class="orange">(855) 631-2544</span></h3>
-            <button id="gethelpbutton" type="button" name="gethelpnow">Get Help Now</button>
+            <h3><?php the_field( 'cta_white_1' ) ?> <span class="orange"><?php the_field( 'cta_orange_1' ) ?></span> <?php the_field( 'cta_white_2' ) ?> <span class="orange"><?php the_field( 'cta_orange_2' ) ?></span></h3>
+
+              <!-- <button id="gethelpbutton" type="button" name="gethelpnow"><?php the_field( 'cta_button_text' ) ?></button> -->
+              <a id="gethelpbutton" class="calltoaction-button" href="<?php the_field( 'cta_button_url' ) ?>"><?php the_field( 'cta_button_text' ) ?></a>
         </div>
       </div>
     </section>
     <section>
       <div class="success-bkgrnd-img">
-        <img src=" <?php bloginfo( 'template_directory' ) ?>/images/jacksonsq_opt.jpg" alt="bloginfo( 'name' )">
+        <img src="<?php the_field( 'success_bkgnd_img' ) ?>" alt="<?php bloginfo( 'name' )?>">
       </div>
       <div class="success">
         <br>
@@ -104,11 +106,9 @@
         <br>
         <br>
         <div id="success-wrapper" class="container">
-          <h2 id="success-title" class="section-title">WHY WE ARE <span class="orange">SUCCESSFUL</span></h2>
+          <h2 id="success-title" class="section-title"><?php the_field( 'success_title_white' ) ?> <span class="orange"><?php the_field( 'success_title_orange' ) ?></span></h2>
           <hr>
-          <p id="success-text" class="section-text">Our firm handles every aspect of a client’s case, from filing the initial application, to appealing denials, to arguing before Administrative Law Judges. If you are represented by Ascend Disability, every aspect of your case will be handled and monitored by experienced attorneys and staff to ensure that nothing falls through the cracks. Many people who file without the assistance of an attorney are denied, because they do not send in the appropriate paperwork, or the Social Security Administration does not consider all of the evidence in their case. We believe that strong cases are built upon strong foundations, so we work from the very beginning to anchor our clients’ cases to solid rock.<br><br>
-          If you are no longer able to work due to a serious injury or illness that has lasted, or will last for 12 months or longer, you may be entitled to Social Security Disability benefits. These benefits are not public assistance. They are reserved for seriously injured or disabled Americans who have worked and paid their taxes into the system. If you believe that you may be entitled to benefits, please feel free to contact us for a free case evaluation.<br><br>
-          We do not get paid unless you win, and our fee is capped at 25% of any back-pay that you are owed. We do not get a dime of your ongoing, monthly benefits if you are awarded. The consultation is free, so there is nothing to lose.</p>
+          <p id="success-text" class="section-text"><?php the_field( 'success_description' ) ?></p>
           <h3 id="success-button" class="calltoaction-button">Learn More</h3>
         </div>
       </div>
